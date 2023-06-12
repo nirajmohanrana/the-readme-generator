942b3297ef1ff869023029579f987876e50ccdcf
Saurabh Vishwakarma <100016387+SaurabhVishwakarma826@users.noreply.github.com>
2023-06-12 05:58:10 +0000

    adding assets

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 const About = () => {
   return (
     <>
-    <h1 className="text-2xl font-bold">About Us</h1>
       <div>
         {/* LEFT */}
         <img src="" alt="" />
         {/* RIGHT */}
         <div>
-          <h1></h1>
-          <p></p>
+          <h1 className="text-2xl font-bold">About Us</h1>
+          <h3 className="text-1xl font-bold">Readme Genrator</h3>
+          <p>Article evident arrived express highest men did boy. Mistress sensible entirely am so. Quick can manor smart money hopes worth too. Comfort produce husband boy her had hearing. Law others theirs passed but wishes. You day real less till dear read. Considered use dispatched melancholy sympathize discretion led. Oh feel if up to till like.</p>
         </div>
       </div>
 
